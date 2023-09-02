8b16fddd837100bf836d290c3d02c5c063bcae59
Merigaz <davidpdavila@gmail.com>
2023-09-01 23:17:05 -0500

    fixing export route ts nextauth

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -5,7 +5,7 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-export const authOptions = {
+const authOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
