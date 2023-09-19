d40cb5f78e9d6348a619303c78cdc429f839e269
Merigaz <davidpdavila@gmail.com>
2023-09-18 22:20:43 -0500

    adapt form for api map request

--- src/app/components/formAddressComponent.tsx
+++ src/app/components/formAddressComponent.tsx
@@ -9,6 +9,11 @@ export default function FormAddressComponent() {
   const { error, setError } = useContext(ErrorContext);
   const [form] = Form.useForm();
   const [responseData, setResponseData] = useState<VotingPlace[]>([]);
+  const [formValues, setFormValues] = useState({
+    inputaddress1: "",
+    inputaddress2: "",
+    inputaddress3: "",
+  });
   useEffect(() => {
     const pollingPlace = async () => {
       try {
@@ -24,11 +29,16 @@ export default function FormAddressComponent() {
   }, []);
   console.log(responseData);
   const onFinish = (values: FormType) => {
+    const address = `${formValues.inputaddress1}${formValues.inputaddress2}${formValues.inputaddress3}`;
+    const payload = {
+      ...values,
+      addressname: address,
+    };
     try {
       const url = "/address";
       const method = "POST";
-      console.log(values);
-      HandlerFormData(url, method, values);
+      console.log(payload);
+      HandlerFormData(url, method, payload);
     } catch {
       setError(true);
     } finally {
@@ -78,7 +88,29 @@ export default function FormAddressComponent() {
           name="addressname"
           rules={[{ required: true, message: "Digite su dirección!" }]}
         >
-          <Input />
+          <div className="flex flex-row">
+            <Input
+              placeholder="Carrera 68"
+              style={{ width: "120px" }}
+              onChange={(e: React.FormEvent<HTMLInputElement>) =>
+                setFormValues({ ...formValues, inputaddress1: e.currentTarget.value })
+              }
+            />
+            <Input
+              disabled
+              value="#"
+              bordered={false}
+              style={{ color: "#C3B984", width: "36px" }}
+            />{" "}
+            <Input placeholder="44" style={{ width: "58px" }} />{" "}
+            <Input
+              disabled
+              value="-"
+              bordered={false}
+              style={{ color: "#C3B984", width: "32px" }}
+            />{" "}
+            <Input placeholder="44" style={{ width: "58px" }} />
+          </div>
         </Form.Item>
         <Form.Item<FormType>
           label="Barrio"
@@ -100,9 +132,11 @@ export default function FormAddressComponent() {
           rules={[{ required: true, message: "Digite la mesa de votación!" }]}
         >
           <Select>
-          {responseData
-              ? responseData.map((place:VotingPlace) => (
-                  <Select.Option key={place.id} value={place.id}>{place.votingplace}</Select.Option>
+            {responseData
+              ? responseData.map((place: VotingPlace) => (
+                  <Select.Option key={place.id} value={place.id}>
+                    {place.votingplace}
+                  </Select.Option>
                 ))
               : null}
           </Select>
