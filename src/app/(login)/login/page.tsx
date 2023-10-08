4c9ee3d9ca3b9390500f86d9d0a073efb246d15f
Merigaz <davidpdavila@gmail.com>
2023-10-07 20:26:58 -0500

    login container

--- src/app/(login)/login/page.tsx
+++ src/app/(login)/login/page.tsx
@@ -1,3 +1,47 @@
-export default function LoginPage () {
- return ( <div>login</div>)
+"use client"
+import { Button, Card, Form, Input } from "antd";
+
+export default function LoginPage() {
+  return (
+    <div className="flex justify-center align-middle m-auto">
+   
+      <Card bordered={false} >
+        <Form
+          layout="vertical"
+          name="basic"
+          labelCol={{ span: 8 }}
+          style={{ maxWidth: 600 }}
+          initialValues={{ remember: true }}
+          autoComplete="on"
+        >
+          <Form.Item
+            name="username"
+            rules={[
+              {
+                required: true,
+                message: "Por favor ingrese su correo electrónico",
+              },
+            ]}
+          >
+            <Input placeholder="Correo electrónico" />
+          </Form.Item>
+
+          <Form.Item
+            name="password"
+            rules={[
+              { required: true, message: "Por favor ingrese su contraseña" },
+            ]}
+          >
+            <Input.Password placeholder="Contraseña" />
+          </Form.Item>
+
+          <Form.Item style={{ textAlign: "center" }}>
+            <Button type="primary" htmlType="submit">
+              Iniciar sesión
+            </Button>
+          </Form.Item>
+        </Form>
+      </Card>
+    </div>
+  );
 }
